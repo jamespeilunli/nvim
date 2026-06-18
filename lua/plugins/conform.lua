db7bdce0a6a527e68dbd5fb0790955d3d7695a97
James Li <jamespeilunli@gmail.com>
2026-06-17 17:36:24 -0700

    some changes from mac

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,3 @@
 return {
   "stevearc/conform.nvim",
-  opts = function(_, opts)
-    opts.formatters_by_ft.java = { "google_java_format" }
-    opts.formatters_by_ft.python = { "ruff" }
-
-    opts.formatters = vim.tbl_deep_extend("force", opts.formatters or {}, {
-      google_java_format = {
-        command = "google-java-format",
-        args = { "-r", "$FILENAME" },
-        stdin = false,
-      },
-      ruff_format = {
-        command = "ruff",
-        args = { "format", "$FILENAME" },
-        stdin = false,
-      },
-    })
-  end,
 }
